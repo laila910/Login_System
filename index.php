edc3b6df8ce97d98f046b27b20a13878140b7033
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-12 23:43:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require "header.php";
 ?>
 <main>
    <?php
-   if(isset($_SESSION['userId'])){
+   if(!isset($_SESSION['userId'])){
        echo '<p>You are Logged in!</p>';
    }else{
-       echo '<p>You are Logged out!</p>';
+       echo '<p>You are Logged outs!</p>';
    }
 
 ?>
