fcde2f2f1ece1ab27829008a82a97402a60e1a49
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-12 02:20:29 +0200

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -21,16 +21,11 @@ if(isset($_POST['signup-submit'])){
                    if(!filter_var($s_email,FILTER_VALIDATE_EMAIL)){
                         header("Location: ../signup.php?error=InvalidEmail&uid=".$username);
                         exit();
-                    }
-                    
-        
-     }elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)&&!preg_match("/^[a-zA-Z0-9]*$/",$username){
-                 header("Location: ../signup.php?error=InvalidEmailuid");
-                exit();
-      
-
-        
-     } elseif($password !== $passwordRepeat){
+                    } 
+     }elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)&&!preg_match("/^[a-zA-Z0-9]*$/",$username)){
+              header("Location: ../signup.php?error=invalidemailuid");
+              exit();
+     }elseif($password !== $passwordRepeat){
 
               header("Location: ../signup.php?error=passwordcheck&uid=".$username."&email=".$email);
               exit();
@@ -38,19 +33,43 @@ if(isset($_POST['signup-submit'])){
          
                header("Location: ../signup.php?error=InvalidUid&email=".$email);
                exit();
-    
-
-
-     }else{
-          
-
-
-        
-      
-
-
-      
+     }else{ //check if the user enter username is already exist in the database 
+          $sql= "SELECT uidUsers FROM users WHERE uidUsers=?;"; 
+          $stmt=mysqli_stmt_init($conn); //if the connection to the database is opened
+          if(! mysqli_stmt_prepare($stmt,$sql)){ //check if the connection is  open between my query sql statment and the database
+               header("Location: ../signup.php?error=sqlerror");
+               exit();
+          }else{
+              mysqli_stmt_bind_param($stmt,'s',$username); //send the username that the user is already enter it to the database to check
+              mysqli_stmt_execute($stmt);//open the connection 
+              mysqli_stmt_store_result($stmt);//the result of the select statment from the database
+              $resultcheck = mysqli_stmt_num_rows($stmt);//check the number of rows of the result 
+              if($resultcheck > 0 ){
+                   header("Location: ../signup.php?error=usertaken&email".$email);
+                   exit();
+              }else{
+                  $sql = "INSERT INTO users (uidUsers,emailUsers,pwdUsers) VALUES (?,?,?); ";
+                    $stmt=mysqli_stmt_init($conn); //if the connection to the database is opened
+                    if(! mysqli_stmt_prepare($stmt,$sql)){ //check if the connection is  open between my query sql statment and the database
+                         header("Location: ../signup.php?error=sqlerror");
+                         exit();
+                    }else{
+                        $hashedPassword=password_hash($password,PASSWORD_DEFAULT);
+                         mysqli_stmt_bind_param($stmt,'sss',$username,$email,$hashedPassword); 
+                         mysqli_stmt_execute($stmt);
+                         header("Location: ../signup.php?signup=success");
+                         exit();
+                   
+                    }
+              }  
+          }
    }
+   mysqli_stmt_close($stmt);
+   mysqli_close($conn);
+}
+else{
+     header("Location: ../signup.php");
+     exit();
 }
  
 
