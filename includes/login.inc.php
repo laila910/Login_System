403a16a6bab890b1490847f170f3763c037f8607
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-13 22:04:25 +0200

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -2,7 +2,7 @@
 
 
 if(isset($_POST['login-submit'])){
-  require "dbh.inc.php";
+require "dbh.inc.php";
   $mailuid= $_POST['mailuid'];
   $password=$_POST['pwd'];
   if(empty($mailuid)||empty($password)){
@@ -10,22 +10,29 @@ if(isset($_POST['login-submit'])){
        exit();
 
   }else{
-      $sql="SELECT * FROM users where uidUsers=? OR emilUsers=?;";
-      $stmt=mysqli_stmt_init($conn);
-      if(! mysqli_stmt_prepare($stmt,$sql)){
+   
+      $sql="SELECT * FROM users where  emailUsers=? or uidUsers=?;";
+      $stmt= mysqli_stmt_init($conn);
+     
+      if(!mysqli_stmt_prepare($stmt,$sql)){
        header("Location: ../index.php?error=sqlerror");
        exit();
        
       }else{
-          mysqli_stmt_bind_param($stmt,'ss',$mailuid,$mailuid);
+          mysqli_stmt_bind_param($stmt,"ss",$mailuid,$password);
           mysqli_stmt_execute($stmt);
           $result=mysqli_stmt_get_result($stmt);
+
           if($row = mysqli_fetch_assoc($result)){
-              $pwdcheck=password_verify($password,$row['pwdUsers']);
-              if($pwdcheck==false){
+         
+             
+            //   $pwdcheck = password_verify($password,$row['pwdUsers']);
+              if($password !== $row['pwdUsers']){
+    
                   header("Location: ../index.php?error=wrongpwd");
+
                   exit();
-              }elseif($pwdcheck==true){
+              }elseif($password === $row['pwdUsers']){
                  session_start();
                  $_SESSION['userId']= $row['idUsers'];
                  $_SESSION['userUid'] = $row['uidUsers'];
@@ -50,3 +57,7 @@ if(isset($_POST['login-submit'])){
       header("Location: ../index.php");
      exit();
 }
+echo mysqli_connect_error($conn);
+      exit();
+
+    ?>
