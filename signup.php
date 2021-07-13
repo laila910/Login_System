9a20b7d56fde413b3151e965edf122cf22cb8024
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-13 22:36:18 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,6 +5,32 @@ require "header.php";
 ?>
 <main>
     <h1>Sign Up</h1>
+    <?php 
+    if(isset($_GET['error'])){
+        if($_GET['error']=="emptyfields"){
+            echo'<p> Fill in all fields!</p>';
+        }elseif($_GET['error']=="invalidemailuid"){
+            echo'<p>Invalid username and email </p>';
+
+        }elseif($_GET['error']=="InvalidUid"){
+            echo'<p>Invalid username </p>';
+
+        }elseif($_GET['error']=="InvalidEmail"){
+            echo'<p>Invalid E-mail </p>';
+
+        }elseif($_GET['error']=="passwordcheck"){
+            echo'<p>Your Passwords are not match!</p>';
+
+        }elseif($_GET['error']=="usertaken"){
+            echo'<p>Username is already taken! </p>';
+
+        }
+    } elseif(isset($_GET['signup'])){
+          if($_GET['signup']=="success"){
+           echo'<p>You are sign-up! </p>';
+    }}
+
+    ?>
     <form action="includes/signup.inc.php" method="post">
         <input type="text" name="uid" placeholder="Enter UserName">
         <input type="text" name="mail" placeholder="Enter Your E-mail">
